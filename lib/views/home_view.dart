6f7e5def8c5e871d4e7579f89168feaf6a54ae63
Faizah-55 <faizahalyasi@gmail.com>
2025-05-19 11:40:23 +0300

    hi git

---
+++ lib/views/home_view.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:getx_lab_2/controller/home_controller.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final HomeController controller = Get.put(HomeController());
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Center(
+        // child: GetX<HomeController>(
+        //   builder: (controller) {
+        //     return Column(
+        //       mainAxisAlignment: MainAxisAlignment.center,
+        //       children: <Widget>[
+        //         const Text('you have pushed the buttom this many times'),
+        //         Text(
+        //           '${controller.counter}',
+        //           style: Theme.of(context).textTheme.headlineMedium,
+        //         ),
+        //         ElevatedButton(
+        //           onPressed: () {
+        //             controller.incremenCounter();
+        //           },
+        //           child: Text('+'),
+        //         ),
+        //         ElevatedButton(
+        //           onPressed: () {
+        //             controller.incremenCounter();
+        //           },
+        //           child: Text('-'),
+        //         ),
+        //       ],
+        //     );
+        //   },
+        child: Obx(
+          () => Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text('you have pushed the buttom this many times'),
+              Text(
+                '${controller.counter}',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  controller.incremenCounter();
+                },
+                child: Text('+'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  controller.incremenCounter();
+                },
+                child: Text('-'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
